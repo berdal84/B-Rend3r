8f6f3b7a95c3b7352a605e1b0f9d63bcd57889ef
socrastination <contact@dalle-cort.fr>
2016-06-26 16:47:47 +0200

    Source unzipped

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: socrastination
+ *
+ * Created on 26 juin 2016, 15:49
+ */
+
+#include <cstdlib>
+#include <stdio.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("Turtl3D v0.1");
+    
+    return 0;
+}
+
