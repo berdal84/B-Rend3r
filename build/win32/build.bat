f54e15df1e82968a2edd4e6231a8f580595ace36
berdal84 <contact@dalle-cort.fr>
2017-11-21 21:33:37 +0100

    linux64 - new build script to build only with g++

--- build/win32/build.bat
+++
@@ -1,5 +0,0 @@
-:: Compiles and link
-g++ -std=c++11 ../../src/*.cpp ../../main.cpp -o ../../bin/win32/B-Rend3r.exe -Wall -mwindows ../../lib/glew32.lib -lopengl32 -lmingw32 -lSDL2main -lSDL2
-:: Copy last version of shaders
-ROBOCOPY /MIR "../../shaders/" "../../bin/win32/shaders/"
-pause
--- src/Transform.cpp
+++ src/Transform.cpp
@@ -1,4 +1,5 @@
 #include "Transform.h"
+#include <cmath>
 
 Transform::Transform():
     position(0.f, 0.f, 0.f),
